5109edb9d3a80e40fc7c9698d28127054574b7f5
WASDetchan <yarik.antipov2008@gmail.com>
2026-01-16 17:36:55 +0300

    Make the build script async

--- build.lua
+++ build.lua
@@ -1,8 +1,25 @@
 local function notify(msg, level)
+  -- print("notified: ", msg)
   coroutine.yield({
     msg = msg,
-    level = level or vim.log.levels.TRACE,
+    level = level or vim.log.levels.INFO,
   })
+end
+
+local to_notify = {}
+
+local function notify_add(msg, level)
+  table.insert(to_notify, { msg = msg, level = level or vim.log.levels.INFO })
+end
+
+local function notify_all()
+  local tab
+  to_notify, tab = {}, to_notify
+  for _, msg in pairs(tab) do
+    -- print("notifiying all: msg = ", msg.msg)
+    notify(msg.msg, msg.level)
+  end
+  -- print("notified all")
 end
 
 local function lib_names()
@@ -28,22 +45,51 @@ local function build()
 
   if not src_name then return end
 
-  notify("Building (cargo build --release)…")
+  notify("Building (cargo build --release --locked)…")
 
-  local err = nil
+  local uv = vim.uv
+  local stderr = uv.new_pipe()
+  local return_code = nil
 
-  vim.system(
-    { "cargo", "build", "--release" },
-    { cwd = root, text = true },
-    function(res)
-      if res.code ~= 0 then
-        err = res.stderr or "cargo build failed"
-        return
-      end
+  local handle, _ = uv.spawn(
+    "cargo",
+    {
+      cwd = root,
+      args = { "build", "--release", "--locked" },
+      stdio = { nil, nil, stderr }
+    },
+    function(code, _)
+      -- notify_add("Finished building with code " .. code)
+      return_code = code
     end
-  ):wait()
-  if err then
-    notify(err, vim.log.levels.ERROR)
+  )
+
+  if not stderr or not handle then
+    notify("Failed to spawn build process!", vim.log.levels.ERROR)
+    return
+  end
+
+  stderr:read_start(function(_, chunk)
+    -- print("got chunk of stderr: ", chunk)
+    if chunk then
+      notify_add(chunk)
+    end
+  end)
+
+  while not return_code do
+    if (#to_notify > 0) then
+      notify_all()
+      -- print("notified all")
+    end
+    -- vim.wait(100)
+    coroutine.yield()
+  end
+  notify_all()
+
+
+  if return_code ~= 0 then
+    notify("Build failed with exit code " .. return_code, vim.log.levels.ERROR)
+    return
   else
     notify("Built " .. dst_name)
   end
@@ -57,7 +103,7 @@ local function build()
     return
   end
 
-  print("dst_dir: ", dst_dir)
+  -- print("dst_dir: ", dst_dir)
   vim.fn.mkdir(dst_dir, "p")
   vim.fn.delete(dst)
   vim.fn.writefile(vim.fn.readfile(src, "b"), dst, "b")
